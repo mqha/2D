485cda76caccf12e1f014be7e900e0c0c71e24fc
MQHA\Administrator <hunganh7793@gmail.com>
2023-12-27 20:55:40 +0700

    no message

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speedMove = 10f;
+
+    private float horizontal = 0f;
+
+    private Rigidbody2D rigidbody2D;
+
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horizontal, 0f, 0f);
+
+        transform.position += movement * speedMove * Time.deltaTime;
+
+        Flip(horizontal);
+    }
+
+    private void Flip(float horizontal)
+    {
+        if((horizontal > 0 && transform.localScale.x < 0)||(horizontal < 0 && transform.localScale.x > 0))
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.x *= -1;
+            transform.localScale = newScale;
+        }
+    }
+}
