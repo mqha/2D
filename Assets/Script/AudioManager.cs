1bffc324f4fcf53bf83e8457cf0610f00fb68509
FT-TS03227\Admin <hunganh7793@gmail.com>
2024-01-18 16:07:41 +0700

    no message

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] musicSounds;
+
+    public AudioSource musicSource;
+}
