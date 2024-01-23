258e03cc502990fc2814833950d6c556f3b83037
FT-TS03227\Admin <hunganh7793@gmail.com>
2024-01-23 16:19:14 +0700

    no message

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -9,24 +9,37 @@ public class ScoreManager : MonoBehaviourSingletonPersistent<ScoreManager>
     public TMP_Text scoreText;
     public TMP_Text highScoreText;
 
-
-
-     int score = 0;
-     int highscore = 0;
-
+    private int score = 0;
+    private int highscore = 0;
 
     private void Start()
     {
-        highscore = PlayerPrefs.GetInt("highscore",0);
-        scoreText.text = score.ToString() + " Points";
-        highScoreText.text = "HIGHSCORE: " + highscore.ToString();
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        highScoreText.text = "Highscore: " + highscore.ToString();
     }
 
     public void AddPoint()
     {
-        score +=1;
-        scoreText.text = score.ToString() + " Points";
+        score += 1;
         if (highscore < score)
-            PlayerPrefs.SetInt("highscore", score);       
+        {
+            highscore = score;
+            PlayerPrefs.SetInt("highscore", score);
+        }
+        UpdateUI();
     }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+
+
 }
